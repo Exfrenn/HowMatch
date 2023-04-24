5d73d9940fc86043d5f28317f3d689292e96c42d
Exfrenn <122411993+Exfrenn@users.noreply.github.com>
2023-04-23 20:38:24 -0600

    Add files via upload

---
+++ HM.js
@@ -0,0 +1,9 @@
+function j_search(){
+window.open("BusquedaS.HTML","body");
+}
+function redirectsmo(){
+window.open("SMO.HTML","body");
+}
+function redirectpco(){
+window.open("PCO.HTML","body");
+}
